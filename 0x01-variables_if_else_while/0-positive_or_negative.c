920f1289319dc89d56a12da49130993b67a9b2b1
Ameena Murtala <murtalaameena@gmail.com>
2023-08-12 03:52:35 -0700

    Write a program that prints the alphabet in lowercase, and then in uppercase, followed by a new line.

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
- * main - generate random numbers to detect -ve or +ve
+ * main - Entry point
  *
- * Return: always (0)
- *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int n;
-
-	srand('time'(0));
-	n'rand'() - RAND_MAX / 2;
-	if (n > 0)
-	{
-		printf("%d is positive\n", n);
-	}
-	else
-		| (n --0);
-	{
-		printf("%d is zero\n", n);
-	}
-	if else
-	{
-		printf("%d is negative\n", n);
-	}
-	return (0);
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+/* your code goes there */
+if (n > 0)
+{
+printf("%d is positive\n", n);
+}
+if (n < 0)
+{
+printf("%d is negative\n", n);
+}
+if (n == 0)
+{
+printf("%d is zero\n", n);
+}
+return (0);
 }
